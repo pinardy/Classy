a8d28d1c7a0c66f63855a1ca6f15a006b779f4e6
Pinardy <pinardy@hotmail.com>
2016-12-03 12:22:14 +0800

    Added base drawing app

---
+++ EveryNote/app/src/main/java/com/example/pin/everynote/DrawView.java
@@ -0,0 +1,84 @@
+package com.example.pin.everynote;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Pin on 03-Dec-16.
+ */
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawView extends View {
+    // setup initial color
+    private final int paintColor = Color.BLACK;
+    // defines paint and canvas
+    private Paint drawPaint;
+    // stores next circle
+    private static Path path = new Path();
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    private void setupPaint() {
+        // Setup paint with color and stroke styles
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+        // Checks for the event that occurs
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(pointX, pointY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+        // Force a view to draw again
+        postInvalidate();
+        return true;
+    }
+
+    public void clearCanvas() {
+        path.reset();
+        invalidate();
+    }
+}
